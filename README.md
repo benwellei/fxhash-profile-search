bc93b189cf8b0a6216bce96cff4ab1058fa97683
benwellei <benwellei@protonmail.com>
2022-01-31 13:53:31 -0800

    prompt for address, output link to profile

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fxhash-profile-search
 Checks to see if a wallet is registered on FXhash
+
+1) `npm install`
+2) run `node index.js`
+3) paste tezos address when prompted
--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 import fetch from "node-fetch";
+import * as readline from 'node:readline';
 
-const address = 'tz1UPfsrzqrJ2DrYnZdrVsjBohtxxyQ8fK2e';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-fetch(`https://api.tzkt.io/v1/contracts/KT1Ezht4PDKZri7aVppVGT4Jkw39sesaFnww/bigmaps/users_name/keys?limit=10000`)
+rl.question('\nWhat Tezos address would you like to check?\n', (address) => {
+    fetch(`https://api.tzkt.io/v1/contracts/KT1Ezht4PDKZri7aVppVGT4Jkw39sesaFnww/bigmaps/users_name/keys?limit=10000`)
     .then(res => res.json())
     .then(res => {
-        console.log(`There are ${res.length} registered wallets`)
+        console.log(`\nThere are ${res.length} registered wallets\n`)
         res.forEach(a => {
             if (a.key == address) {
                 const value = a.value;
 
                 console.log(`Found address ${address}`);
                 console.log(`The username associated with this address is ${hexToUtf8(value)}`);
+                console.log(encodeURI(`https://www.fxhash.xyz/u/${hexToUtf8(value)}`))
             }
         });
-    });
+    })
+    .then(() => {rl.close();})
+});
 
 function hexToUtf8 (hex) {
     return decodeURIComponent(
